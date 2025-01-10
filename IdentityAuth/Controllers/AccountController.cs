1ee40aedac6fb633614079fe63b4bba1f584b3a9
Admin <Admin@DESKTOP-E1COFAC>
2025-01-10 10:48:22 +0700

    Add project files.

---
+++ IdentityAuth/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAuth.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class AccountController : Controller
+    {
+        // Action cho trang đăng ký
+        public IActionResult Register()
+        {
+            return View("/Areas/Identity/Pages/Account/Register.cshtml");
+        }
+        // Action cho trang đăng nhập
+        public IActionResult Login()
+        {
+            return View("/Areas/Identity/Pages/Account/Login.cshtml");
+        }
+    }
+}
---
+++ IdentityAuth/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using IdentityAuth.Data;
+using IdentityAuth.Areas.Identity.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+var connectionString = builder.Configuration.GetConnectionString("IdentityAuthContextConnection") ?? throw new InvalidOperationException("Connection string 'IdentityAuthContextConnection' not found.");
+
+builder.Services.AddDbContext<IdentityAuthContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityAuthUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<IdentityAuthContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();   
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
